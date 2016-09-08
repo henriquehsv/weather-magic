0230ff80edc73dec5b9b7e53bdb32eec6ce6d52d
Henrique Vasconcelos <henriquehsv@gmail.com>
2016-09-08 14:08:26 -0300

    Adding action to settings menus

--- app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -1,11 +1,14 @@
 package com.example.android.sunshine.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 public class DetailActivity extends AppCompatActivity {
@@ -21,5 +24,28 @@ public class DetailActivity extends AppCompatActivity {
         DetailActivityFragment detailActivityFragment = new DetailActivityFragment();
         detailActivityFragment.setArguments(getIntent().getExtras());
         supportFragmentManager.beginTransaction().add(R.id.fragment_area, detailActivityFragment).commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //noinspection SimplifiableIfStatement
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+
     }
 }
